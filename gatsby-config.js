3e8dc7d84dae08fdf5d824844d7f940c583bce69
Sachin Jain <sachin.jain2@mindtree.com>
2018-04-04 17:12:40 +0530

    nprogress setup

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet', {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `yellow`,
+        // Disable the loading spinner.
+        showSpinner: true
+      }
+    }
+  ]
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+var express = require("express");
+var path = require('path');
+var app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
+var port = process.env.PORT || 5050;
+var server = app.listen(port, function() {
+  console.log("Server is running on port " + port);
+});
